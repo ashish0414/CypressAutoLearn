f02e922b9ce3d9d47caa38767b0e6e4e20a926ab
Ashish <ashish.kumar@aceturtle.com>
2022-03-20 22:09:46 +0530

    "format code with ientation"

--- cypress/integration/AjioIntegrationSmokeTest/ForwardOrderFlow.js
+++ cypress/integration/AjioIntegrationSmokeTest/ForwardOrderFlow.js
@@ -5,46 +5,46 @@ import DashboardPage from '../../support/pageObjectsRubicon3/DashboardPage'
 import SideBarPage from '../../support/pageObjectsRubicon3/SideBarPage'
 import ShipmentFastPanelPage from '../../support/pageObjectsRubicon3/ShipmentFastPanelPage'
 
-describe('Ajio Forward Order Flow test with Rubicon Fast Panel',()=>{
+describe('Ajio Forward Order Flow test with Rubicon Fast Panel', () => {
 
-   // let loginPage = new LoginPage()
+    // let loginPage = new LoginPage()
     let dashboardPage = new DashboardPage()
     let sidebarPage = new SideBarPage()
     let shipmentFastPanelPage = new ShipmentFastPanelPage()
 
     let order_no;
     let shipment_no;
-    before(function(){
+    before(function () {
         //cy.visit(Cypress.env('MB_stg_url'))
-        cy.fixture('AjioPayloads').then((ajioData)=>{
-            this.ajioData=ajioData
+        cy.fixture('AjioPayloads').then((ajioData) => {
+            this.ajioData = ajioData
             this.ajioData.orderPayload.orderId = `${Math.floor(Math.random() * 100000000000)}`
             order_no = this.ajioData.orderPayload.orderId
             this.ajioData.orderPayload.shipment_number = `${Math.floor(Math.random() * 100000)}`
-            shipment_no=this.ajioData.orderPayload.shipment_number
+            shipment_no = this.ajioData.orderPayload.shipment_number
             this.ajioData.orderPayload.po_number = `${Math.floor(Math.random() * 1000000)}`
             this.ajioData.orderPayload.orderDate = new Date()
         })
 
     })
 
-    beforeEach(function(){
+    beforeEach(function () {
 
-        cy.fixture('example').then(function(testData){
+        cy.fixture('example').then(function (testData) {
 
-            this.testData=testData
-            
+            this.testData = testData
+
         })
-       
-       
+
+
 
 
 
     })
 
-    it('Create Ajio Orders',function(){
+    it('Create Ajio Orders', function () {
 
-        
+
 
         //cy.log(JSON.stringify(this.ajioData.orderPayload))
         cy.log('ashish singh')
@@ -54,72 +54,71 @@ describe('Ajio Forward Order Flow test with Rubicon Fast Panel',()=>{
             method: 'POST',
             url: 'https://in.data.aceturtle.in/staging/ajio/create_shipment', // baseUrl is prepend to URL
             headers: {
-                "x-api-key" :"GZfbkFLnil29isBFPfxTr3mLANMQy9Lx18ITiima"
+                "x-api-key": "GZfbkFLnil29isBFPfxTr3mLANMQy9Lx18ITiima"
             },
             form: false, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
             body: this.ajioData.orderPayload,
-          }).then((res)=>{
-              expect(res.status).to.eql(200)
-              
-          })
-          cy.wait(10000)
+        }).then((res) => {
+            expect(res.status).to.eql(200)
+
+        })
+        cy.wait(10000)
     })
 
-    it('Login to R3 and processing the Ajio Order till its delivered',function(){
-        
-        
-       //using custom commands from support/command.js passing username , password(from fixtures/example) and url from cypress.json in the params
+    it('Login to R3 and processing the Ajio Order till its delivered', function () {
 
-        cy.login(this.testData.MB_staging_login.username,this.testData.MB_staging_login.password,Cypress.env('MB_stg_url'))
 
-       
+        //using custom commands from support/command.js passing username , password(from fixtures/example) and url from cypress.json in the params
+
+        cy.login(this.testData.MB_staging_login.username, this.testData.MB_staging_login.password, Cypress.env('MB_stg_url'))
+
+
 
 
 
         //clicking on the side bar page
-        
-        dashboardPage.getSideBarToggeleButton({force: true}).click()
-        sidebarPage.clickFastPanel().click({force: true})
-       
-    
+        dashboardPage.getSideBarToggeleButton({ force: true }).click()
+        sidebarPage.clickFastPanel().click({ force: true })
+
+
 
         //Navigating to shipment Fast panel
-        
 
-        sidebarPage.getShipmentFastPanel().click({force: true})
-       
-        
+
+        sidebarPage.getShipmentFastPanel().click({ force: true })
+
+
 
         //checking the order_no
-            shipmentFastPanelPage.getPageHeading().should('have.text',"Process Orders")
-            shipmentFastPanelPage.getOrderNoSearchEditBox().type(order_no)
+        shipmentFastPanelPage.getPageHeading().should('have.text', "Process Orders")
+        shipmentFastPanelPage.getOrderNoSearchEditBox().type(order_no)
 
-            shipmentFastPanelPage.getSearchButton().click()
+        shipmentFastPanelPage.getSearchButton().click()
 
-            //shipmentFastPanelPage.getAcceptCookiesButton().click()
+        //shipmentFastPanelPage.getAcceptCookiesButton().click()
 
-            shipmentFastPanelPage.getAcceptOrderButton().click()
+        shipmentFastPanelPage.getAcceptOrderButton().click()
 
-            shipmentFastPanelPage.getStatusChangedMsg().should('have.text',"Order status changed successfully")
+        shipmentFastPanelPage.getStatusChangedMsg().should('have.text', "Order status changed successfully")
 
-            shipmentFastPanelPage.getOrderNo().should('have.attr', 'title', order_no)
+        shipmentFastPanelPage.getOrderNo().should('have.attr', 'title', order_no)
 
-            shipmentFastPanelPage.getChannelName().should('have.attr', 'title', 'Ajio')
+        shipmentFastPanelPage.getChannelName().should('have.attr', 'title', 'Ajio')
 
-            //Printing the Invoice
-            cy.wait(10000)
-            cy.pause()
-            shipmentFastPanelPage.getPrintInvoiceBtnOnAcceptSuccess().invoke('show')
-            cy.contains('Print Invoice/Shipping Label').click()
+        //Printing the Invoice
+        cy.wait(10000)
+        cy.pause()
+        shipmentFastPanelPage.getPrintInvoiceBtnOnAcceptSuccess().invoke('show')
+        cy.contains('Print Invoice/Shipping Label').click()
 
-        
+
 
 
 
 
     })
 
-    
+
 
 
 
